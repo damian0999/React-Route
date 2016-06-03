0297eec9908dcdc60e3e0958e6d7e52b5cfa1858
Damian <labasdamian@gmail.com>
2016-06-03 18:44:02 +0200

    Route between files

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import {Router, Route, Link, hashHistory} from 'react-router';
-
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
-
-const Links = () =>
-  <nav>
-    <Link to='/'>Home <br /> </Link>
-    <Link to='/about'>About <br /> </Link>
-    <Link to='/contact'>Contact <br /> </Link>
-  </nav>
+import {Link} from 'react-router';
 
 class App extends React.Component{
   render(){
     return(
-      <Router history={ hashHistory }>
-        <Route path='/' component={Home}></Route>
-        <Route path='/about' component={About}></Route>
-        <Route path='/contact' component={Contact}></Route>
-      </Router>
+      <div>
+        <h1>React Router - Home</h1>
+        <ul>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+
+        {this.props.children}
+      </div>
     );
   }
 }
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './app';
+import About from './about'
+import Contact from './contact'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <Route path='/about' component={About}/>
+      <Route path='/contact' component={Contact}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
